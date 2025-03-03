8c56346389dbe95cb62e7b6113cb7eee274c0930
tahsinrefat <tahsin92refat@gmail.com>
2025-03-04 00:11:20 +0600

    provided the public db credentials

--- tms-backend/src/main/java/com/example/tms/utils/PasswordEncoderImpl.java
+++ tms-backend/src/main/java/com/example/tms/utils/PasswordEncoderImpl.java
@@ -7,7 +7,7 @@ public class PasswordEncoderImpl {
     public static void main(String[] args) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-        System.out.println(passwordEncoder.encode("tahsin"));
+//        System.out.println(passwordEncoder.encode("tahsin"));
         System.out.println(passwordEncoder.encode("password"));
     }
 }
